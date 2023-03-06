3bf538a0684540a68171a96fe6bf246b0976b857
guadalupe <ggonzalezq@ecoquintas.com>
2023-03-05 19:59:26 -0600

    Genera token cuando ingresa usuario

--- api/modelo/db_usuarios.py
+++ api/modelo/db_usuarios.py
@@ -1,13 +1,16 @@
 from modelo.conexion import CL_Conexion
-
+import hashlib
+import secrets
 class CL_UsuarioDB:
 
     def FN_NuevoUsuario(self,usuario):
         usuario = usuario['Usuario']
+        usuario['token'] = secrets.token_hex(16)
+        usuario['token'] = hashlib.md5(usuario['token'].encode())
         query = """INSERT INTO Users 
-				(email, first_name, last_name, idRoles) 
-				VALUES (%s, %s, %s, %s)"""
-        val = (usuario['email'],usuario['first_name'],usuario['last_name'],usuario['idRoles'])
+				(email, first_name, last_name, idRoles, token) 
+				VALUES (%s, %s, %s, %s, %s)"""
+        val = (usuario['email'],usuario['first_name'],usuario['last_name'],usuario['idRoles'], usuario['token'].hexdigest())
         usu = CL_Conexion().set_DB(query, val)
         usuario['id'] = usu
         usuario['Usuario'] = usuario
