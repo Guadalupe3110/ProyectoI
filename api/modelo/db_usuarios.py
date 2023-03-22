66d976a32737a4aa46f551128fb5e068513ad6a2
guadalupe <ggonzalezq@ecoquintas.com>
2023-03-21 21:29:15 -0600

    contrasena encriptada

--- api/modelo/db_usuarios.py
+++ api/modelo/db_usuarios.py
@@ -1,13 +1,15 @@
 from modelo.conexion import CL_Conexion
+import hashlib
 class CL_UsuarioDB:
 
     #Insertar en la base de datos un nuevo usuario
     def FN_NuevoUsuario(self,usuario):
+        contrasena = hashlib.md5(usuario['password'].encode())
         usuario = usuario['Usuario']
         query = """INSERT INTO Users 
 				(email, first_name, last_name, idRoles, password) 
 				VALUES (%s, %s, %s, %s, %s)"""
-        val = (usuario['email'],usuario['first_name'],usuario['last_name'],usuario['idRoles'],usuario['password'])
+        val = (usuario['email'],usuario['first_name'],usuario['last_name'],usuario['idRoles'],contrasena.hexdigest())
         usu = CL_Conexion().set_DB(query, val)
         usuario['id'] = usu
         usuario['Usuario'] = usuario
@@ -15,8 +17,9 @@ class CL_UsuarioDB:
     
     #Select para el login de un usuario con inner join para saber el rol de usuario logueado
     def FN_Login(self, email, contrasena):
+        contrasena = hashlib.md5(contrasena.encode())
         query = "SELECT u.*, r.* FROM Users as u  INNER JOIN Roles AS r ON u.idRoles  = r.idRoles WHERE u.email = %s AND u.password = %s"
-        val = (email, contrasena)
+        val = (email, contrasena.hexdigest())
         self.data = CL_Conexion().get_DB_value(query, val)
         if len(self.data) != 0:
             return self.data[0]
